491b95afe25e5d33a2c4d991ea46ad3391e97db4
Ayush Deshmukh <ayushmdcr@gmail.com>
2025-04-10 00:04:10 +0530

    github issues ftw

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.expensive"
-
-    compileSdk = 33
-    ndkVersion = "27.0.12077973"
+    namespace = "com.example.new_expense_app"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -20,15 +20,20 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.expensive"
-        minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.new_expense_app"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     buildTypes {
         release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
